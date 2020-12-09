9042148060f56f107f79aeff175c6fa6382cc0c3
Jorge Simões <63779047+JLuzioS@users.noreply.github.com>
2020-12-09 22:13:59 +0000

    radixSort() fixed

--- src/serie2/ListUtils.java
+++ src/serie2/ListUtils.java
@@ -7,7 +7,7 @@ public class ListUtils {
     public static <E> void removeAfterIntersectionPoint( Node<E> list1, Node<E> list2, Comparator<E> cmp) {
         Node<E> val1 = list1.previous, val2 = list2.previous;
         while (val1 != list1 && val2 != list2){
-            if (val1.value != val2.value){
+            if (cmp.compare(val1.value, val2.value) != 0){
                 val1.next = list1;
                 list1.previous = val1;
                 return;
