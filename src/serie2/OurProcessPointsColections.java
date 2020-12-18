b88e5049ddfb0029028dd829fd3822bac7b447fb
Tiago Silva <tiagosik1994@gmail.com>
2020-12-18 22:11:02 +0000

    refactor point class from ProcessPointsColections and OurProcessPointsColections

--- src/serie2/OurProcessPointsColections.java
+++ src/serie2/OurProcessPointsColections.java
@@ -2,35 +2,11 @@ package serie2;
 
 import java.io.*;
 import java.util.Iterator;
-import java.util.Objects;
 import java.util.Scanner;
 
 import static java.lang.System.nanoTime;
 
 public class OurProcessPointsColections {
-    private static class Point {
-        String x;
-        String y;
-
-        public Point(String x, String y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Point point = (Point) o;
-            return x.equals(point.x) &&
-                    y.equals(point.y);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(x, y);
-        }
-    }
 
     public static void main(String[] args) {
 
---
+++ src/serie2/Point.java
@@ -0,0 +1,27 @@
+package serie2;
+
+import java.util.Objects;
+
+class Point {
+    String x;
+    String y;
+
+    public Point(String x, String y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return x.equals(point.x) &&
+                y.equals(point.y);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+}
